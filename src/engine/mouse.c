d573157d840a622dc713cbe90f1485a383217b88
roger-rab-bit <ruggero-dolzi@virgilio.it>
2024-04-26 04:00:29 +0200

    mouse rotation [done]

--- src/engine/mouse.c
+++ src/engine/mouse.c
@@ -6,7 +6,7 @@
 /*   By: rdolzi <rdolzi@student.42roma.it>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 01:21:50 by flaviobiond       #+#    #+#             */
-/*   Updated: 2024/04/26 03:27:36 by rdolzi           ###   ########.fr       */
+/*   Updated: 2024/04/26 03:59:09 by rdolzi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,26 @@
 
 int	mouse_control(int x,int y, t_game *game)
 {
-	printf("x:%d", x);
-	printf("y:%d", y);
-	if (x < 0)
+	int half_width;
+	(void)y;
+	// printf("x:%d ", x);
+	// printf("y:%d", y);
+	
+	half_width = (int )WIN_WIDTH / 2;
+	if (x > half_width - 60 && x < half_width + 60)
 	{
-		printf("gira a sinistra\n");
-		game->player.rotate -= 0.1; 
+		// printf("rimane fermo\n");
+		game->player.rotate = 0;
+	}
+	else if (x < half_width - 40)
+	{
+		// printf("gira a sinistra\n");
+		game->player.rotate = -0.45; 
 	}
 	else
 	{
 		printf("gira a destra\n");
-		game->player.rotate += 0.1; 
+		game->player.rotate = 0.45; 
 	}
 	return (0);
 }
